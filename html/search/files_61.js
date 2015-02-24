00a3080b9433fb851e8076a441ff1c92c71be3d1
ruslan <ruslan363809785@gmail.com>
2015-02-24 12:49:38 +0400

    Revert "lab2"
    This reverts commit 6946f8b1c6e526abffbab832249fda6cdb1821ba.

--- html/search/files_61.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['aa_2ecpp',['aa.cpp',['../aa_8cpp.html',1,'']]]
-];
--- html/search/functions_71.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['quicksort',['quickSort',['../aa_8cpp.html#a8db79f6529b0a74f21b62ab9281c57e6',1,'aa.cpp']]]
-];
